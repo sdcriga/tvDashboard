b71819b481d6754917045bbc82bfef2a497d7986
Ilze-R <irigerte@gmail.com>
2024-04-26 15:02:36 +0300

    port changed

--- src/app/_service/data-save.service.ts
+++ src/app/_service/data-save.service.ts
@@ -9,7 +9,7 @@ import { Observable, catchError, tap, throwError } from 'rxjs';
 })
 export class DataSaveService {
   //  private readonly server: string = 'http://localhost';
- private readonly server: string = 'http://screen.local:8080';
+ private readonly server: string = 'http://screen.local:9090';
 
   constructor(private http: HttpClient) {}
   
